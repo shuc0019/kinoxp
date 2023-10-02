d8107a83f39d44117f0ca6a702b2fef85ba8b024
shuc0019 <shuc0019@stud.kea.dk>
2023-10-02 13:44:45 +0200

    s

--- src/main/java/com/example/kinoxpproject/Bio.java
+++
@@ -1,2 +0,0 @@
-package com.example.kinoxpproject;public class Bio {
-}
---
+++ src/main/java/com/example/kinoxpproject/entity/Movie.java
@@ -0,0 +1,8 @@
+package com.example.kinoxpproject.entity;
+
+public class Movie {
+    private String name;
+    private String description;
+    private long length;
+    private String genre;
+}
